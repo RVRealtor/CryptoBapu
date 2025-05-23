06e65be35e5aa9f524839c2209a07c1ba4ae4f55
RVRealtor <ravishrealtor@gmail.com>
2025-05-23 16:54:11 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,1 @@
-/*
-export { auth as middleware } from './auth'
-
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
-}
-*/
+Remove middleware code to disable login wall
